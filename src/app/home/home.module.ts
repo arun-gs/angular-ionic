0a6c831b4d49efd992bfe7e61e0140c2f8bc3e69
Arun G <mail@arun-g.in>
2022-09-13 09:56:24 +0530

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { UserDetailsComponent } from 'src/app/omponents/user-details/user-details.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -14,6 +15,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    UserDetailsComponent
+]
 })
 export class HomePageModule {}
